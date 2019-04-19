02795f37c5b2c4304817428a922cdfc542b2d9b5
robertfairley <rrafairley@gmail.com>
2019-04-19 16:20:38 -0400

    [ Patch ] Refine layout exports, and combine under AppleNews namespace.

---
+++ src/article-layout/index.ts
@@ -0,0 +1,5 @@
+export { Anchor } from "./anchor";
+export { AutoPlacementLayout } from "./auto-placement-layout";
+export { ComponentLayout } from "./component-layout";
+export { ConditionalComponentLayout } from "./conditional-component-layout";
+export { Layout } from "./layout";
