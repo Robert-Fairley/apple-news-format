bea0a901006adfa431725f4f579a1e8007021566
robertfairley <rrafairley@gmail.com>
2019-04-19 16:38:08 -0400

    [ Patch ] Started adding grouped object exports for ArticleDocument before realizing which branch I was on. Keeping the changes anyway.

--- src/article-layout/index.ts
+++ src/article-layout/index.ts
@@ -3,3 +3,7 @@ export { AutoPlacementLayout } from "./auto-placement-layout";
 export { ComponentLayout } from "./component-layout";
 export { ConditionalComponentLayout } from "./conditional-component-layout";
 export { Layout } from "./layout";
+
+export interface ComponentLayouts {
+  [key: string]: ComponentLayout;
+}
