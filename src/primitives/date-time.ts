690c3987e057a3f345dde8d2beda9fc0a728f87f
Robert Fairley <rrafairley@gmail.com>
2019-04-22 22:39:19 -0400

    [ Patch ] Add reference for ISO 8601 date standard

--- src/primitives/date-time.ts
+++ src/primitives/date-time.ts
@@ -6,6 +6,9 @@ export type DateTime = string;
 /**
  * Validation function for UTC ISO 8601 DateTime string. Returns string if valid,
  * undefined if invalid.
+ * @see https://en.wikipedia.org/wiki/ISO_8601
+ * @see https://www.iso.org/standard/70907.html [ISO 8601-1:2019]
+ * @see https://www.iso.org/standard/70908.html [ISO 8601-2:2019]
  * @param {DateTime} s
  * @returns {DateTime|undefined}
  * @todo Adjust matching regex to more explicitly match ISO spec for UTC timezone designation. Or
