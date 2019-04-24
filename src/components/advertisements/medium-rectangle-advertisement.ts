ffbe8abe5d0aa2c6957d0e1dfccf82bf146972d5
robertfairley <rrafairley@gmail.com>
2019-04-23 20:02:04 -0400

    [ Patch ] Add advertisment components

---
+++ src/components/advertisements/medium-rectangle-advertisement.ts
@@ -0,0 +1,10 @@
+import { Component } from "../component";
+
+/**
+ * Signature/interface for a `MediumRectangleAdvertisement` object
+ * @see https://developer.apple.com/documentation/apple_news/mediumrectangleadvertisement
+ * @extends {Component}
+ */
+export interface MediumRectangleAdvertisement extends Component {
+  role: "medium_rectangle_advertisement";
+}
