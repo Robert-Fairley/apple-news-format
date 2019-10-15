2a23c08b3463fffc5ebd77776fd121e8d03c520d
gpoitch <garth22@gmail.com>
2019-10-15 10:46:59 -0400

    Add caption field for all image components

--- src/components/images/image-component.ts
+++ src/components/images/image-component.ts
@@ -1,6 +1,7 @@
 import { URI } from "../../primitives";
 import { Component } from "../component";
 import { ComponentLink } from "../component-link";
+import { CaptionDescriptor } from "./caption-descriptor";
 
 /**
  * Possible values for image component `role` fields
@@ -20,6 +21,7 @@ export interface ImageComponent extends Component {
   role: ImageComponentRole;
   URL: URI;
   accessibilityCaption?: string;
+  caption?: CaptionDescriptor | string;
   additions?: ComponentLink[];
   explicitContent?: boolean;
 }
