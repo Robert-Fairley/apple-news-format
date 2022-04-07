035d82ee43357801789ce0082e86cf4272ebf33a
Nic Barajas <nicolas.barajas@bustle.com>
2022-04-07 19:20:10 +0000

    add contentDisplay to conditional container

--- src/components/article-structure/conditional-container.ts
+++ src/components/article-structure/conditional-container.ts
@@ -4,6 +4,8 @@ import { Condition } from "../../primitives";
 import { ComponentStyle } from "../../styles/component-styles/component-style";
 import { Behavior } from "../behavior";
 import { ComponentAnimation } from "../component-animation";
+import { CollectionDisplay } from "./collection-display";
+import { HorizontalStackDisplay } from "./horizontal-stack-display";
 
 /**
  * Signature/interface for a `ConditionalContainer` object
@@ -14,6 +16,7 @@ export interface ConditionalContainer {
   anchor?: Anchor;
   animation?: ComponentAnimation;
   behavior?: Behavior;
+  contentDisplay?: CollectionDisplay | HorizontalStackDisplay;
   hidden?: false;
   layout?: ComponentLayout;
   style?: ComponentStyle;
