b727571ef55f3e12d05a0843bb9b77f353482d4e
Nic Barajas <nicolas.barajas@bustle.com>
2022-04-07 19:35:13 +0000

    extend conditional component

--- src/components/article-structure/conditional-container.ts
+++ src/components/article-structure/conditional-container.ts
@@ -1,9 +1,4 @@
-import { Anchor } from "../../article-layout";
-import { ComponentLayout } from "../../article-layout/component-layout";
-import { Condition } from "../../primitives";
-import { ComponentStyle } from "../../styles/component-styles/component-style";
-import { Behavior } from "../behavior";
-import { ComponentAnimation } from "../component-animation";
+import { ConditionalComponent } from "../conditional-component";
 import { CollectionDisplay } from "./collection-display";
 import { HorizontalStackDisplay } from "./horizontal-stack-display";
 
@@ -11,13 +6,6 @@ import { HorizontalStackDisplay } from "./horizontal-stack-display";
  * Signature/interface for a `ConditionalContainer` object
  * @see https://developer.apple.com/documentation/apple_news/conditionalcontainer
  */
-export interface ConditionalContainer {
-  conditions: Condition | Condition[];
-  anchor?: Anchor;
-  animation?: ComponentAnimation;
-  behavior?: Behavior;
+export interface ConditionalContainer extends ConditionalComponent {
   contentDisplay?: CollectionDisplay | HorizontalStackDisplay;
-  hidden?: false;
-  layout?: ComponentLayout;
-  style?: ComponentStyle;
 }
