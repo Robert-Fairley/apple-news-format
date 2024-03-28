4e3d06e5175df13ef1d53ca1ce0cfa7eef7b9741
shaw-t <trae.shaw@wanews.com.au>
2024-03-28 15:49:54 +0800

    Deprecate auto placement layout

--- src/article-layout/auto-placement-layout.ts
+++ src/article-layout/auto-placement-layout.ts
@@ -3,6 +3,7 @@ import { Margin } from "../primitives";
 /**
  * Signature/interface for an `AutoPlacementLayout` object
  * @see https://developer.apple.com/documentation/apple_news/autoplacementlayout
+ * @deprecated
  */
 export interface AutoPlacementLayout {
     margin?: Margin | number; // Integer
