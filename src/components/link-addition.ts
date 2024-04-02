1706bc4a7001fa759d4ba9fdf774902e697d49b4
shaw-t <trae.shaw@wanews.com.au>
2024-04-02 14:36:13 +0800

    Update link-addition to support required fields - rangeLength updated to required - rangeStart updated to required

--- src/components/link-addition.ts
+++ src/components/link-addition.ts
@@ -6,9 +6,12 @@ import { Addition } from "./addition";
 
 /**
  * Signature/interface for a `Link` object
- * @see https://developer.apple.com/documentation/apple_news/link
+ * @see https://developer.apple.com/documentation/apple_news/linkaddition
  * @extends {Addition}
  */
 export interface LinkAddition extends Addition {
+  type: "link";
   URL: URI | IdentifierURI;
+  rangeLength: number; // Integer
+  rangeStart: number; // Integer
 }
